c4ccd058525f1fd57c51af2f5f21a2e916538212
JacJac <46572814+jacpeery@users.noreply.github.com>
2019-01-24 16:22:01 -0600

    Stubbed out web caesar

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
-from flask import Flask
+from flask import Flask, request
+from caesar import rotate_string
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+form = value of html
+
 @app.route("/")
 def index():
-    return "Hello World, It's Jac and I am back to coding"
+    return "Hello World, It's Jac and I am back to coding!"
 
 app.run()
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+        form {
+            background-color: #eee;
+            padding: 20px;
+            margin: 0 auto;
+            width: 540px;
+            font: 16px sans-serif;
+            border-radius: 10px;
+        }
+        textarea {
+            margin:10px 0;
+            width: 540px;
+            height: 120px;                
+        }
+        <style>
+    </head>
+
+    <body>
+        <! -- create your form here -->
+
+    </body>   
+         
+</html>
+
